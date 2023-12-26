5f15a9b96c374841273afdb3ec3341105991fa8f
Shazzadul-Shakib <shakib1186@gmail.com>
2023-12-26 23:13:00 +0600

    sidebar added without responsiveness

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -7,15 +7,13 @@ export default function protectedLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main>
+    <main className="grid h-[100dvh] grid-rows-[auto_1fr_auto] gap-1 sm:gap-6'">
       <Navbar />
-      <section className="flex flex-col md:flex-row md:overflow-hidden">
-        <div className="basis-1/6 ">
+      <section className="flex flex-col h-full md:flex-row md:overflow-hidden">
+        <div className="basis-1/6">
           <Sidebar />
         </div>
-        <div className="basis-5/6 flex-grow md:overflow-y-auto">
-          {children}
-        </div>
+        <div className="basis-5/6 flex-grow md:overflow-y-auto">{children}</div>
       </section>
     </main>
   );
