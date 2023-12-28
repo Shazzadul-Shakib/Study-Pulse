1e80d5e34af2c08338f590b78df4eb1111c6a971
Shazzadul-Shakib <shakib1186@gmail.com>
2023-12-28 20:18:46 +0600

    create reusable modalbody and card to create group

--- app/(protected)/practice_ground/page.tsx
+++ app/(protected)/practice_ground/page.tsx
@@ -1,7 +1,15 @@
-export default function PracticeGround() {
+import React from "react";
+import ModalBody from "@/app/components/modals/modalbody";
+import CreateGroupCard from "@/app/components/cards/createGroupCard";
+
+const PracticeGround: React.FC = () => {
+  // const card: JSX.Element = <CreateGroupCard/>;
+
   return (
-    <main>
-      <h1>Practice Ground page</h1>
+    <main className="h-full">
+      {/* <ModalBody card={card} /> */}
     </main>
   );
-}
+};
+
+export default PracticeGround;
