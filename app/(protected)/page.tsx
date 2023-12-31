e069d5674db3bfb04348fd05e502372a01d4ed88
Shazzadul-Shakib <shakib1186@gmail.com>
2023-12-31 19:17:14 +0600

    get all study groups from database

--- app/(protected)/page.tsx
+++ app/(protected)/page.tsx
@@ -1,15 +1,27 @@
+import connectMongoDB from "@/lib/mongoDB/mongo-client";
 import StudyGroupCard from "../components/cards/studyGroupCard";
 import CreateGroup from "../components/createGroup";
+import studyGroup from "@/lib/models/create-group-schema";
 
-export default function StudyGourps() {
+export default async function StudyGourps() {
+  await connectMongoDB();
+  const studyGroups = await studyGroup.find();
+  
   return (
     <main className="h-full shadow-inner p-2 shadow-deepestBlue">
-      <CreateGroup />
-      {/* <div className=" w-[90%] mt-6 mx-auto grid grid-cols-3 gap-">
-        <StudyGroupCard />
-        <StudyGroupCard />
-        <StudyGroupCard />
-      </div> */}
+      <CreateGroup props={studyGroups.length} />
+      
+      <div className=" w-[90%] mt-6 mx-auto grid grid-cols-3 gap-">
+        {studyGroups.map((studyGroup) => {
+          return (
+            <StudyGroupCard
+              key={studyGroup._id}
+              groupName={studyGroup.groupName}
+              topicName={studyGroup.topicName}
+            />
+          );
+        })}
+      </div>
     </main>
   );
 }
--- app/components/cards/studyGroupCard.tsx
+++ app/components/cards/studyGroupCard.tsx
@@ -2,7 +2,7 @@ import backgroundImageSrc from "@/public/sp.png";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function StudyGroupCard() {
+export default function StudyGroupCard({...props}) {
   return (
     <Link href='/' className="relative h-44 w-72 rounded-md shadow-lg overflow-hidden">
       <Image
@@ -16,9 +16,9 @@ export default function StudyGroupCard() {
       <section className="absolute inset-0 flex flex-col justify-end ">
         <div className=" w-full backdrop-blur-md p-4 text-center">
           <h1 className="  text-white text-lg font-bold">
-            Quantum Galaxers of React
+            {props.groupName}
           </h1>
-          <h3 className="text-white text-xs font-bold ">Chemistry</h3>
+          <h3 className="text-white text-xs font-bold ">{props.topicName}</h3>
         </div>
       </section>
     </Link>
