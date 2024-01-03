00eaaeaaa4538e670e076a6e6db7a8beee2b16d8
Shazzadul-Shakib <shakib1186@gmail.com>
2024-01-03 21:01:21 +0600

    Created the toggle modal for creating challanges

--- app/(protected)/study_groups/[id]/challanges/page.tsx
+++ app/(protected)/study_groups/[id]/challanges/page.tsx
@@ -1,8 +1,26 @@
-export default function Challanges({ params }) {
+"use client";
+
+import CreateChallangeCard from "@/app/components/cards/createChallangeCard";
+import ModalBody from "@/app/components/modals/modalbody";
+import { useToggleModal } from "@/hooks/useToggleModal";
+
+export default function Challanges({ params }:{params:{id:string}}) {
   const { id } = params;
+  const [isOpen, toggleModal] = useToggleModal();
   return (
     <main>
-      <h1 className="text-white">challangesfor {id}</h1>
+      <div className=" flex justify-end">
+        <button
+          onClick={toggleModal}
+          className="px-2 py-1 rounded-md text-xs text-white bg-gradient-to-tr from-gradientRightBlue to-gradientLeftBlue "
+        >
+          Create Challange
+        </button>
+        {isOpen && (
+          <ModalBody card={<CreateChallangeCard toggleModal={toggleModal} />} />
+        )}
+      </div>
+      <section></section>
     </main>
   );
 }
